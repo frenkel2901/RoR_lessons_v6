376bb5746619fb1e5785b3fb2e50363ebaca04e8
frenkel2901 <a.frenkel2901@gmail.com>
2022-11-12 16:19:54 +0300

    add: foreign keys

--- db/migrate/20221111152846_create_questions.rb
+++ db/migrate/20221111152846_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-      t.string :body
+      t.string :body, null: false
+      t.references :test, null: false, foreign_key: { to_table: :tests}
 
       t.timestamps
     end
--- db/migrate/20221111152923_create_answers.rb
+++ db/migrate/20221111152923_create_answers.rb
@@ -1,8 +1,9 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.text :body
-      t.boolean :correct
+      t.text :body, null: false
+      t.boolean :correct, default: false, null: false
+      t.references :question, null: false, foreign_key: { to_table: :questions}
 
       t.timestamps
     end
