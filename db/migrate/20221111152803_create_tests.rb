0962994d270e802bd7079d2de4144f92d71bbce2
frenkel2901 <a.frenkel2901@gmail.com>
2022-11-13 15:24:52 +0300

    rewrite to_table

--- db/migrate/20221111152803_create_tests.rb
+++ db/migrate/20221111152803_create_tests.rb
@@ -3,8 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: 0, null: false
-      t.references :category, null: false, foreign_key: { to_table: :categories}
-      t.references :author, null: false, foreign_key: { to_table: :users}
+      t.references :category, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users } 
 
       t.timestamps
     end
