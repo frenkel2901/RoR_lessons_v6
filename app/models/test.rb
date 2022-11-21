12e9766c1b709deb63228ded9d80d6afe4b70140
frenkel2901 <a.frenkel2901@gmail.com>
2022-11-21 18:19:53 +0300

    add: relationships

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+  belongs_to :author, class_name: "User"
+
   def self.c_sort(c_title)
     joins("JOIN categories ON categories.id = tests.category_id").where(categories: { title: c_title })
       .order(id: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+  has_many :made_tests, class_name: "Test"
+  
   def my_tests(level)
     Test.joins("JOIN results ON tests.id = results.test_id").where(results: { user_id: id }, level: level)
   end
