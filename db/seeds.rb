dc8d75f2a0a4d1117c45e61730fd45d99767b901
frenkel2901 <a.frenkel2901@gmail.com>
2022-11-22 12:06:57 +0300

    fix: db/seeds.rb now without ID

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,45 +21,45 @@ users = User.create!([{ name: 'Anna' }, { name: 'Josef' }, { name: 'John' }])
 
 tests = Test.create!(
   [
-    { title: 'HTML Basics', level: 0, category_id: categories[1].id, author_id: users[0].id },
-    { title: 'Ruby Basics', level: 0, category_id: categories[0].id, author_id: users[1].id },
-    { title: 'HTML Medium', level: 1, category_id: categories[1].id, author_id: users[0].id },
-    { title: 'Ruby Advanced', level: 2, category_id: categories[0].id, author_id: users[1].id },
-    { title: 'SQL Advanced', level: 2, category_id: categories[2].id, author_id: users[2].id }
+    { title: 'HTML Basics', level: 0, category: categories[1], author: users[0] },
+    { title: 'Ruby Basics', level: 0, category: categories[0], author: users[1] },
+    { title: 'HTML Medium', level: 1, category: categories[1], author: users[0] },
+    { title: 'Ruby Advanced', level: 2, category: categories[0], author: users[1] },
+    { title: 'SQL Advanced', level: 2, category: categories[2], author: users[2] }
   ]
 )
 
 questions = Question.create!(
   [
-    { body: 'Which element supports all spaces in text?', test_id: tests[0].id },
-    { body: 'What is the <br> element used for?', test_id: tests[2].id },
-    { body: 'What is the difference between puts and print?', test_id: tests[1].id },
-    { body: 'short form x = x / 3', test_id: tests[3].id },
-    { body: 'What are relational databases?', test_id: tests[4].id }
+    { body: 'Which element supports all spaces in text?', test: tests[0] },
+    { body: 'What is the <br> element used for?', test: tests[2] },
+    { body: 'What is the difference between puts and print?', test: tests[1] },
+    { body: 'short form x = x / 3', test: tests[3] },
+    { body: 'What are relational databases?', test: tests[4] }
   ]
 )
 
 answers = Answer.create!(
   [
-    { body: '<pre>', correct: true, question_id: questions[0].id },
-    { body: '<br>', correct: false, question_id: questions[0].id },
-    { body: 'To wrap lines', correct: false, question_id: questions[1].id },
-    { body: 'To create empty space', correct: false, question_id: questions[1].id },
-    { body: 'puts without line break', correct: false, question_id: questions[2].id },
-    { body: 'puts with a line break', correct: false, question_id: questions[2].id },
-    { body: 'x/3', correct: false, question_id: questions[3].id },
-    { body: 'x /= 3', correct: false, question_id: questions[3].id },
-    { body: 'Any database', correct: false, question_id: questions[4].id },
-    { body: 'A database in which information is stored in the form of two-dimensional tables linked together', correct: false, question_id: questions[4].id }    
+    { body: '<pre>', correct: true, question: questions[0] },
+    { body: '<br>', correct: false, question: questions[0] },
+    { body: 'To wrap lines', correct: false, question: questions[1] },
+    { body: 'To create empty space', correct: false, question: questions[1] },
+    { body: 'puts without line break', correct: false, question: questions[2] },
+    { body: 'puts with a line break', correct: false, question: questions[2] },
+    { body: 'x/3', correct: false, question: questions[3] },
+    { body: 'x /= 3', correct: false, question: questions[3] },
+    { body: 'Any database', correct: false, question: questions[4] },
+    { body: 'A database in which information is stored in the form of two-dimensional tables linked together', correct: false, question: questions[4] }  
   ]
 )
 
 results = Result.create!(
   [
-    { user_id: users[0].id, test_id: tests[0].id },
-    { user_id: users[0].id, test_id: tests[3].id },
-    { user_id: users[1].id, test_id: tests[2].id },
-    { user_id: users[1].id, test_id: tests[3].id },
-    { user_id: users[2].id, test_id: tests[4].id }
+    { user: users[0], test: tests[0] },
+    { user: users[0], test: tests[3] },
+    { user: users[1], test: tests[2] },
+    { user: users[1], test: tests[3] },
+    { user: users[2], test: tests[4] }
   ]
 )
