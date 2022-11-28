9d3204c2f241fb54d91ac5ab3de5f8cadd966028
frenkel2901 <a.frenkel2901@gmail.com>
2022-11-28 17:07:57 +0300

    redo lesson6 after corrections

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :tests, dependent: :destroy
 
-  scope :c_default, -> { orded(:title) }
-
   validates :title, persence: true
+
+  default_scope { orded(:title) }
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -6,17 +6,17 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
 
-  scope :easy, -> { where(level: 0..1) }
-  scope :medium, -> { where(level: 2..4) }
-  scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :t_category, lambda { |c_title|
-    joins(:category).where(categories: { title: c_title }).order(id: :desc)
-  }
-
   validates :title, persence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def self.c_sort(c_title)
-    t_category(c_title).pluck(:title)
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :category, lambda { |title|
+    joins(:category).where(categories: { title: title }).order(id: :desc)
+  }
+
+  def self.sort_by_category(title)
+    category(title).pluck(:title)
   end
 end
