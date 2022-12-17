faa0b7635ec972fe4bfbb2425e0fbd6d737cd530
frenkel2901 <a.frenkel2901@gmail.com>
2022-12-17 21:05:44 +0300

    feat: add percentage of correct answers

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,24 +1,43 @@
 class TestPassage < ApplicationRecord
+
+  GREAT = 85
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_current_question
 
   def completed?
     current_question.nil?
+  end
+
+  def high_mark?
+    percentage_of_correct_answers >= GREAT
+  end
+
+  def percentage_of_correct_answers
+    correct_questions * 100 / self.test.questions.count.to_f
+  end
+
+  def current_question_number
+    test.questions.index(current_question).next
+  end
+
+  def number_of_questions
+    test.questions.count
   end
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
 
     self.current_question = next_question
-    save! unless self.completed?
+    save!
   end
 
   private
 
-  def before_validation_set_first_question
+  def before_validation_set_current_question
     self.current_question = if new_record?
                               test.questions.first
                             else
