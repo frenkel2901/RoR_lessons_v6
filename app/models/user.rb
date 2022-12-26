b9a0839ac8c5885ecda07045a3cfc7dd5513f757
frenkel2901 <a.frenkel2901@gmail.com>
2022-12-26 20:43:51 +0300

    feat: add gem 'bcrypt'

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :made_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
+  has_secure_password
+
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
