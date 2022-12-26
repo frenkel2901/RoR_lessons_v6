25d5a27cf53feee6023ee631dee1418164510d81
frenkel2901 <a.frenkel2901@gmail.com>
2022-12-26 20:19:49 +0300

    start lesson10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :test_passages
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :made_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
-
-  validates :mail, presence: true
-  validates :name, presence: true
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
@@ -13,4 +15,5 @@ class User < ApplicationRecord
   def my_tests(level)
     tests.where(level: level)
   end
+
 end
