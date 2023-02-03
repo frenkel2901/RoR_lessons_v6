93c17190208abc38e1ea91b1854b486458d8ef20
frenkel2901 <a.frenkel2901@gmail.com>
2023-02-03 18:43:00 +0300

    fix: mail --> email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,11 @@ class User < ApplicationRecord
          :rememberable,
          :validatable
 
-  MAIL = /\A[\w]+@([A-z0-9]+[.])+[A-z]{2,4}\z/
-
   has_many :test_passages
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :made_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
-  validates :mail, uniqueness: true, format: { with: MAIL, message: "example@mail.com" }
-  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
