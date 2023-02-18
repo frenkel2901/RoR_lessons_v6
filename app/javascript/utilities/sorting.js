aa5308d7abed0263719a856e867d3d48f3deec6b
frenkel2901 <a.frenkel2901@gmail.com>
2023-02-18 14:05:03 +0300

    add task for lesson 15

--- app/javascript/utilities/sorting.js
+++ app/javascript/utilities/sorting.js
@@ -1,11 +1,59 @@
 document.addEventListener('turbolinks:load', function () {
-  var control = document.querySelector('.sort-by-title')
-
-  control.addEventListener('click', sortRowsByTitle)
+  const control = document.querySelector('.sort-by-title')
+  if (control) {
+    control.addEventListener('click', sortRowsByTitle) }
 })
 
 function sortRowsByTitle() {
-  var table = document.querySelector('table')
+  const table = document.querySelector('table')
 
-  var rows = table.querySelector('tr')
+  // Nodelist
+  // https://developer.mozilla.org/ru/docs/Web/API/NodeList
+  const rows = table.querySelectorAll('tr')
+  const sortedRows = []
+
+  //select all table rows except the first one which is the header
+  for (var i = 1; i < rows.length; i++) {
+    sortedRows.push(rows[i])
+  }
+
+  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+    sortedRows.sort(compareRowsAsc)
+    this.querySelector('.octicon-arrow-up').classList.remove('hide')
+    this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+    this.querySelector('.octicon-arrow-up').classList.add('hide')
+  }
+
+  const sortedTable = document.createElement('table')
+  const tBody = document.createElement('tbody')
+  sortedTable.classList.add('table')
+  tBody.appendChild(rows[0])
+
+  for (var i = 0; i < sortedRows.length; i++) {
+    tBody.appendChild(sortedRows[i])
+  }
+
+  sortedTable.appendChild(tBody)
+  table.parentNode.replaceChild(sortedTable, table)
+}
+
+function compareRowsAsc(row1, row2) {
+  const testTitle1 = row1.querySelector('td').textContent
+  const testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return -1 }
+  if (testTitle1 > testTitle2) { return 1 }
+  return 0;
+}
+
+function compareRowsDesc(row1, row2) {
+  const testTitle1 = row1.querySelector('td').textContent
+  const testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 < testTitle2) { return 1 }
+  if (testTitle1 > testTitle2) { return -1 }
+  return 0;
 }
