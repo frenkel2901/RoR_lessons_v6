2c05d106248c9b3421299df83e4784eebe017bad
frenkel2901 <a.frenkel2901@gmail.com>
2023-02-24 16:31:31 +0300

    fix naming

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,7 +2,7 @@ class FeedbacksController < ApplicationController
 
   def create
     unless params[:comment].nil?
-      FeedbackMailer.send_feedback(current_user, params[:comment]).deliver_now
+      FeedbacksMailer.send_feedback(current_user, params[:comment]).deliver_now
       redirect_to root_path, { notice: "Сообщение отправлено" }
     end
   end
--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,10 +1,10 @@
 class FeedbacksMailer < ApplicationMailer
 
   def send_feedback(current_user, comment)
-    @user = current_user
+    @email = current_user.email
     @comment = comment
     @admin_email = ENV['ADMIN_EMAIL']
-    mail to: @admin_email, subject: "Сообщение от пользователя #{@user.email}"
+    mail to: @admin_email, subject: "Сообщение от пользователя #{@email}"
   end
 
 end
